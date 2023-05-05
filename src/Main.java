57c40b1170188360ff4f30dc45dec8ec6e088e1e
buithimaixuan <xuanbui2272003@gmail.com>
2023-05-05 21:30:53 +0700

    basic

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+                
+////        String s = "Gfg 9 + 6 = 12.0";
+//
+//        // create a new scanner
+//        // with the specified String Object
+//        Scanner scanner = new Scanner(System.in);
+//
+//        while (scanner.hasNext()) {
+//
+//            // if the next is a Int,
+//            // print found and the Int
+//            if (scanner.hasNextInt()) {
+//                System.out.println("Found Int value :"
+//                        + scanner.nextInt());
+//            }
+//
+//            // if no Int is found,
+//            // print "Not Found:" and the token
+//            else {
+//                System.out.println("Not found Int value :"
+//                        + scanner.next());
+//            }
+//        }
+//        scanner.close();
+    }
+    }
